704cf1f26dcfeb2f531ea2272f3576cc97029eb5
KARUNONIH <89448748+KARUNONIH@users.noreply.github.com>
2021-10-20 21:34:28 +0700

    Add files via upload

---
+++ Tugas 7/index.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <title>Muhammad Gibran Anggalana</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body>
+        <div class="menu">
+          <a href="">Home</a>
+          <a href="">Profile</a>
+          <a href="">Contact</a>
+          
+        </div>
+
+        <div class="banner">
+          <h1>Artikel Rubik</h1>
+          <p>Dilansir dari Wikipedia Kubus Rubik adalah sebuah permainan teka-teki mekanik yang ditemukan pada tahun 1974 oleh pemahat dan profesor arsitektur Hongaria,
+
+Ernő Rubik. Kubus ini terbuat dari plastik dan terdiri atas 26 bagian kecil yang berputar pada poros yang terlihat. Kamu kembali dan dipasarkan di rubukkawasan eropa pada Mei 1980. Rubik tercatat sebagai mainan yang paling banyak terjual di dunia, dengan sekitar 300 juta kubus Rubik, termasuk imitasinya. </p>
+        </div>
+
+        <div class="body">
+            <div id="b_left">
+                <h5>Jenis - jenis rubik </h5>
+                <h6>Berdasarkan ukuran :</h6>
+                <ul type="circle">
+                    <li>2 x 2 x 2 ( Rubik mini )</li>
+                    <li>3 x 3 x 3 ( Rubik standar )</li>
+                    <li>4 x 4 x 4 ( Master cube )</li>
+                    <li>5 x 5 x 5 ( professor's cube)</li>
+                    <li>6 x 6 x 6 ( V-Cube 6 )</li>
+                    <li>7 x 7 x 7 ( V-Cube 7 )</li>
+                </ul>
+                <h6>Berdasarkan bentuk :</h6>
+                <ul type="disc">
+                    <li>Rubik kubus</li>
+                    <li>rubik segitiga (Pyraminx)</li>
+                    <li>rubik segi delapan (Skewb Diamond)</li>
+                    <li>rubik segi duabelas (Megaminx)</li>
+                </ul>
+            </div>
+            <div id="b_right">
+                <h5>Pertandingan dan rekor rubik</h5>
+                <h6>Speedcubing/speedsolving</h6>
+                <p><b>Speedcubing</b> adalah mencoba untuk menyelesaikan Kubus Rubik secepat mungkin.
+                   Pertandingan Dunia pertama diadakan oleh Guinness Book of World Records yang diadakan di Munich pada 13 Maret 1981. Semua rubik di lubrikasi dengan ''petroleum jelly''. Pemenang resmi, dengan catatan waktu 38 detik adalah Jury Froeschl dari Munich. Kejuaraan dunia pertama internasional diadakan di Budapest pada tanggal 5 Juni 1982, dan dimenangkan oleh Minh Thai, pelajar Vietnam dari Los Angeles, dengan catatan waktu 22,95 detik. </p>
+                <h6>Blindfolded solving</h6>
+                <p><b>Blindfolded solving</b> (sering disingkat dengan BLD)adalah cara menyelesaikan Rubik dengan mata tertutup. Dalam menyelesaikan dengan tertutup, kontestan mememorisasi/mengingat posisi warna Rubik yang teracak dengan mata terbuka, dan kemudian ditutup matanya sebelum menyelesaikan Rubik. Waktu mulai dihitung sejak cuber melihat rubiknya dan melakukan memorisasi. rekor resmi dunia saat ini untuk 3x3x3 BLD adalah 17,20 detik oleh Jeff Park dari Amerika Serikat.
+
+                    Selama waktu penyelesaian, peserta menggunakan penutup mata / blindfold dan seorang juri memegang sehelai kertas di bawah hidung peserta. Hal ini bertujuan agar peserta tidak dapat mengintip di bawah celah blindfold dan hidungnya. </p>
+                    
+                    <h6>Rekor dunia</h6>
+                    <p><b>Rekor dunia</b> dalam menyelesaikan Kubus Rubik dengan mata tertutup berhasil dicetak pada kompetisi Jakarta Open 2010 pada tanggal 31 Januari 2010 di FX Building, Jakarta. Rubik berukuran 3x3 dengan mata tertutup jumlah terbanyak yaitu 16 buah dalam waktu 57 menit diselesaikan atas nama Muhammad Iril Khairul Anam dari Indonesia. Selain itu, Indonesia juga pernah memecahkan rekor dunia lain, dicabang Rubik Clock atas nama Jonathan Irvin Gunawan yang dicetaknya pada 26 May 2012 di Gedung Indonesia Menggugat, Bandung. Dia menyelesaikan Rubik Clock dengan waktu rata-rata 7.09 detik. Semua rekor ini tercatat di WCA. </p>
+            </div>
+        </div>
+
+        <div class="footer">
+          <a href="">Home</a>
+          <a href="">Profile</a>
+          <a href="">Contact</a>
+    </body>
+</html>
